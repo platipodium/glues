b64d5fb53993d0ffc0a55a5efa9f193f23b2800f
Carsten Lemmen <carsten.lemmen@hzg.de>
2009-08-21 11:12:15 +0200

    Added convenience scripts for testing differetn compileres gcc and pgi on gprsrv07

---
+++ pgi.sh
@@ -0,0 +1,4 @@
+./bootstrap && ./configure CC=pgcc CXX=pgCC \
+--with-sisi-include=$HOME/opt/src/SiSi2.2/sisi/include \
+--with-sisi-lib=$HOME/opt/src/SiSi2.2/sisi/lib-pgi \
+&& (cd src ; gmake -k clean ) &&  gmake -k -j4 && ./run.sh
