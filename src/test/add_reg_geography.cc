1a359f9bf2c70adee72468314d674d81d042b3b4
Carsten Lemmen <carsten.lemmen@hzg.de>
2009-09-04 17:04:25 +0200

    Added scripts for creation of standard netcdf in CF convention

---
+++ src/test/add_reg_geography.cc
@@ -0,0 +1,112 @@
+/* GLUES add_reg_geography; this file is part of
+   the Global Land Use and technological Evolution Simulator
+   
+   Copyright (C) 2009
+   Carsten Lemmen <carsten.lemmen@gkss.de>
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 2, or (at your option) any later
+   version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+   Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  
+*/
+/**
+   @author Carsten Lemmen <carsten.lemmen@gkss.de>
+   @date   2009-08-07
+   @file create_reg_climate.cc
+*/
+
+#include "config.h"
+#include <string>
+#include <iostream>
+#include <fstream>
+
+#ifdef HAVE_NETCDF_H
+#include "netcdfcpp.h"
+#endif
+
+using namespace std;
+
+int main(int argc, char* argv[]) 
+{
+
+#ifndef HAVE_NETCDF_H
+    std::cout << " No netcdf interface defined. FAIL" << std::endl;
+    return 1;
+#else
+ 
+  unsigned int i;
+  string filename="reg_geography.nc";
+   
+  NcError ncerror(NcError::verbose_nonfatal);
+  NcFile ncfile(filename.c_str(), NcFile::Write);
+  if (!ncfile.is_valid()) return 1;
+
+  NcAtt* histatt=ncfile.get_att("history");
+  int length=histatt->num_vals();
+  char * cbuff  = new char[length];
+  //for (i=0; i<length; i++) cbuff+i=histatt->as_string(i);
+  cbuff=histatt->as_string(0);
+  string history = "add_reg_gegraphy, " + string(cbuff);
+  histatt->remove();
+  ncfile.add_att("history",history.c_str());
+  
+  NcDim *regdim = ncfile.get_dim("region");
+  int nreg = regdim->size();
+         
+  NcDim *timedim = ncfile.get_dim("time");
+  int ntime = timedim->size();
+
+  string regname = "../../examples/setup/685/region_geography.tsv";
+  ifstream ifs(regname.c_str(),ios::in);  
+  if (ifs.bad()) return 1;  
+
+  ifs.seekg(0,ios::end);
+  length=ifs.tellg();
+  ifs.seekg(0,ios::beg);
+  
+  float *fbuffer=new float[length];
+  while (ifs.good()) ifs >> fbuffer[i++];
+  ifs.close();  
+  
+  length=(i-1)/nreg;
+    
+  float *record=new float[nreg];
+  
+  NcVar *latvar=ncfile.get_var("latitude");
+  if (ncerror.get_err() != 0) {
+    latvar = ncfile.add_var("latitude",ncFloat, regdim);
+    latvar->add_att("units","degrees_north");
+    latvar->add_att("long_name","center_latitude");
+    latvar->add_att("description","Latitude of region center");
+  }
+  for (i=0; i<nreg; i++) record[i]=fbuffer[1+length*i];
+  latvar->put(record,nreg);
+  
+  NcVar *lonvar=ncfile.get_var("longitude");
+  if (ncerror.get_err() !=0 ) {
+    lonvar = ncfile.add_var("longitude",ncFloat, regdim);
+    lonvar->add_att("units","degrees_east");
+    lonvar->add_att("long_name","center_longitude");
+    lonvar->add_att("description","longitude of region center");
+  }
+  for (i=0; i<nreg; i++) record[i]=fbuffer[2+length*i];
+  lonvar->put(record,nreg);
+  
+  delete [] record;
+  delete [] fbuffer;
+ 
+  ncfile.close();  
+       
+  return 0;
+#endif
+}
+
---
+++ src/test/nc_glues.cc
@@ -0,0 +1,111 @@
+/* GLUES nc_glues; this file is part of
+   the Global Land Use and technological Evolution Simulator
+   
+   Copyright (C) 2009
+   Carsten Lemmen <carsten.lemmen@gkss.de>
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 2, or (at your option) any later
+   version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+   Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  
+*/
+/**
+   @author Carsten Lemmen <carsten.lemmen@gkss.de>
+   @date   2009-08-07
+   @file nc_glues.cc
+*/
+
+#include "config.h"
+#include <string>
+
+#ifdef HAVE_NETCDF_H
+#include "netcdfcpp.h"
+#endif
+
+using namespace std;
+
+int main(int argc, char* argv[]) 
+{
+
+#ifndef HAVE_NETCDF_H
+    std::cout << " No netcdf interface defined. FAIL" << std::endl;
+    return 1;
+#else
+ 
+  const int ntime=0;
+  const int nreg=1;
+  string filename="glues_template.nc";
+   
+  NcFile ncfile(filename.c_str(), NcFile::Replace);
+  if (!ncfile.is_valid()) return 1;
+  
+  ncfile.add_att("Conventions","CF-1.4");
+  ncfile.add_att("title","GLUES netcdf template in CF conventions");
+  ncfile.add_att("history","Glues template netcdf file");
+  ncfile.add_att("institution","GKSS-Forschungszentrum Geesthacht GmbH");
+  ncfile.add_att("source","GLUES 1.1.3 model");
+  ncfile.add_att("comment","");
+  ncfile.add_att("references","Wirtz & Lemmen (2003)");
+  
+  ncfile.add_att("model_name","GLUES");
+  
+  NcDim *regdim, *timedim;
+  if (!(regdim  = ncfile.add_dim("region", nreg))) return 1;
+  if (!(timedim = ncfile.add_dim("time", ntime))) return 1;
+       
+  NcVar *regionvar;
+  if (!(regionvar = ncfile.add_var("region", ncFloat, regdim))) return 1;
+  
+  
+  regionvar->add_att("units", "");
+  regionvar->add_att("long_name","region_index");
+  regionvar->add_att("standard_name","region_index");
+  regionvar->add_att("description","Unique integer index of region");
+  regionvar->add_att("valid_min",1.0);
+  regionvar->add_att("coordinates","lon lat");
+  
+  //regionvar->add_att("_FillValue",1E-30);
+
+  NcVar *timevar;
+  timevar   = ncfile.add_var("time", ncFloat, timedim);
+  timevar->add_att("units","years since 01-01-01");
+  timevar->add_att("calendar","360_day");
+  
+  NcVar *latvar;
+  latvar = ncfile.add_var("latitude",ncFloat, regdim);
+  latvar->add_att("units","degrees_north");
+  latvar->add_att("long_name","center_latitude");
+  latvar->add_att("description","Latitude of region center");
+  
+  NcVar *lonvar;
+  lonvar = ncfile.add_var("longitude",ncFloat, regdim);
+  lonvar->add_att("units","degrees_east");
+  lonvar->add_att("long_name","center_longitude");
+  lonvar->add_att("description","longitude of region center");
+  
+  NcVar *techvar;
+  ncfile.add_var("technology",ncFloat,timedim,regdim);
+  //techvar->add_att("long_name","technology_index");
+ 
+  NcVar *farmvar;
+  ncfile.add_var("farming",ncFloat,timedim,regdim);
+//  farmvar->add_att("long_name","farming_ratio");
+ 
+  NcVar *econvar;
+  ncfile.add_var("economies",ncFloat,timedim,regdim);
+  //econvar->add_att("long_name","economy_diversity");
+ 
+       
+  return 0;
+#endif
+}
+
