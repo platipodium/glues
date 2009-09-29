c8ed07edcc8e27e5b44cc91e695a74cdf08e5aa7
Carsten Lemmen <carsten.lemmen@gkss.de>
2009-09-29 10:33:17 +0200

    Added temp file to calc_regionpath

---
+++ visual/matlab/calc_gridcell_area.m
@@ -0,0 +1,14 @@
+function area=calc_gridcell_area(clat,dlon,dlat,radius)
+
+cl_register_function();
+
+if ~exist('radius','var') radius=6378.137; end
+if ~exist('dlon','var') dlon=0.5; end
+if ~exist('dlat','var') dlat=0.5; end
+
+%area=2*pi*radius*radius*(abs(sind(clat+dlat/2.0)));
+area=2*pi*radius*radius*(2*cosd(clat)*sind(dlat/2.0));
+area=area*dlon/360;
+
+return;
+end
