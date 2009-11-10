dc85c2137f7db72342481ad2d90bc2f52ed1986d
Carsten Lemmen <carsten.lemmen@hzg.de>
2009-11-10 17:24:14 +0100

    continued paths adjustment

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-/Users/lemmen/devel/mercurial/src/glues /Users/lemmen/devel/mercurial/examples/simulations/685/lbk.sim
-#src/glues examples/simulations/685_var/lbk.sim
-#ksrc/glues examples/simulations/test_new_event/lbk.sim
-
