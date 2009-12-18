dce938677800ae13eba6de1a8cfba9fb56656ca0
Carsten Lemmen <carsten.lemmen@hzg.de>
2009-12-18 19:13:51 +0100

    Refined test script

--- src/test/test_build_maintainer.sh
+++ src/test/test_build_maintainer.sh
@@ -7,21 +7,55 @@ url="http://glues.hg.sourceforge.net:8000/hgroot/glues/glues"
 dat=`date +"%Y%m%d%H%M" `
 
 owd=`pwd`
-tmp=$HOME/.glues/glues-test-$dat
-log=$HOME/.glues/$dat.log
+topdir=$HOME/.glues
+tmp=$topdir/glues-test-$dat
+log=$topdir/$dat.log
 
-test -d $tmp || mkdir -p $tmp
-cd $tmp
+test -d topdir || mkdir -p $topdir
+test -d $tmp && rm -rf $tmp
 
 echo $dat > $log
-hg clone $url >> $log
-cd glues
-./bootstrap >> $log || echo "Bootstrap failed" >> $log 
-./configure >> $log || echo "Configure failed" >> $log
-./gmake     >> $log || echo "Make failed" >> $log
-sh ./run.sh >> $log || echo "Run failed" >> $log
+
+cd $topdir
+hg clone $url $tmp >> $log
+if ! test -d $tmp ; then 
+  echo "HG checkout faild" >> $log
+  cd $owd
+  exit 1
+fi
+
+cd $tmp
+./bootstrap >> $log 
+if test -x configure ; then : 
+else
+  echo "Bootstrap failed" >> $log
+  cd $owd
+  exit 1
+fi
+  
+./configure >> $log 
+if test -f Makefile ; then :
+else
+  echo "Configure failed" >> $log
+  exit 1
+fi
+  
+gmake -j4  >> $log 
+if ! test -x src/glues ; then
+  echo "Make failed" >> $log
+  cd $owd
+  exit 1
+fi
+
+./run.sh >> $log
+if $? ; then : 
+else 
+  echo "Run failed" >> $log
+  cd $owd
+  exit 1
+fi
 
 cd $owd
 #rm -rf $tmp
 
-exit
+exit 0
