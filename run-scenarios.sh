a9f805a1ade4bcba6884603ed46d6a56c7a70ff8
Carsten Lemmen <carsten.lemmen@hzg.de>
2010-01-07 17:20:38 +0100

    resolved merge conflict in Version

--- run-scenarios.sh
+++ run-scenarios.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+SED=`which sed` 
 X=src/glues
 P=examples/simulations/685/lbk
 S=examples/setup/685
@@ -14,69 +15,69 @@ PAR=$P.par
 
 
 # trade const and migration var, kee0p spreadv*spreadm=0.2
-gsed -i '/spreadv/s/spreadv.*$/spreadv 0.0000002/' $PAR
-gsed -i '/spreadm/s/spreadm.*$/spreadm 1000000/' $OPAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 0.0000002/' $PAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 1000000/' $OPAR
 $X $SIM
 cp ${R}.out ${R}_s7.out
-gsed -i '/spreadv/s/spreadv.*$/spreadv 0.000002/' $PAR
-gsed -i '/spreadm/s/spreadm.*$/spreadm 100000/' $OPAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 0.000002/' $PAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 100000/' $OPAR
 $X $SIM
 cp ${R}.out ${R}_s6.out
-gsed -i '/spreadv/s/spreadv.*$/spreadv 0.00002/' $PAR
-gsed -i '/spreadm/s/spreadm.*$/spreadm 10000/' $OPAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 0.00002/' $PAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 10000/' $OPAR
 $X $SIM
 cp ${R}.out ${R}_s5.out
-gsed -i '/spreadv/s/spreadv.*$/spreadv 0.0002/' $PAR
-gsed -i '/spreadm/s/spreadm.*$/spreadm 1000/' $OPAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 0.0002/' $PAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 1000/' $OPAR
 $X $SIM
 cp ${R}.out ${R}_s4.out
-gsed -i '/spreadv/s/spreadv.*$/spreadv 0.002/' $PAR
-gsed -i '/spreadm/s/spreadm.*$/spreadm 100/' $OPAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 0.002/' $PAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 100/' $OPAR
 $X $SIM
 cp ${R}.out ${R}_s3.out
-gsed -i '/spreadv/s/spreadv.*$/spreadv 0.02/' $PAR
-gsed -i '/spreadm/s/spreadm.*$/spreadm 10/' $OPAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 0.02/' $PAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 10/' $OPAR
 $X $SIM
 cp ${R}.out ${R}_s2.out
-gsed -i '/spreadv/s/spreadv.*$/spreadv 0.2/' $PAR
-gsed -i '/spreadm/s/spreadm.*$/spreadm 1/' $OPAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 0.2/' $PAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 1/' $OPAR
 $X $SIM
 cp ${R}.out ${R}_s1.out
-gsed -i '/spreadv/s/spreadv.*$/spreadv 2/' $PAR
-gsed -i '/spreadm/s/spreadm.*$/spreadm 0.1/' $OPAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 2/' $PAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 0.1/' $OPAR
 $X $SIM
 cp ${R}.out ${R}_s0.out
 
 # only trade variation, migration const
-gsed -i '/spreadv/s/spreadv.*$/spreadv 0.002/' $PAR
-gsed -i '/spreadm/s/spreadm.*$/spreadm 0/' $OPAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 0.002/' $PAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 0/' $OPAR
 $X $SIM
 cp ${R}.out ${R}_v3m0.out
-gsed -i '/spreadm/s/spreadm.*$/spreadm 0.01/' $OPAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 0.01/' $OPAR
 $X $SIM
 cp ${R}.out ${R}_v3m1.out
-gsed -i '/spreadm/s/spreadm.*$/spreadm 0.1/' $OPAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 0.1/' $OPAR
 $X $SIM
 cp ${R}.out ${R}_v3m2.out
-gsed -i '/spreadm/s/spreadm.*$/spreadm 1/' $OPAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 1/' $OPAR
 $X $SIM
 cp ${R}.out ${R}_v3m3.out
-gsed -i '/spreadm/s/spreadm.*$/spreadm 3.3333/' $OPAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 3.3333/' $OPAR
 $X $SIM
 cp ${R}.out ${R}_v3m4.out
-gsed -i '/spreadm/s/spreadm.*$/spreadm 10/' $OPAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 10/' $OPAR
 $X $SIM
 cp ${R}.out ${R}_v3m5.out
-gsed -i '/spreadm/s/spreadm.*$/spreadm 33/' $OPAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 33/' $OPAR
 $X $SIM
 cp ${R}.out ${R}_v3m6.out
-gsed -i '/spreadm/s/spreadm.*$/spreadm 100/' $OPAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 100/' $OPAR
 $X $SIM
 cp ${R}.out ${R}_v3m7.out
-gsed -i '/spreadm/s/spreadm.*$/spreadm 333/' $OPAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 333/' $OPAR
 $X $SIM
 cp ${R}.out ${R}_v3m8.out
-gsed -i '/spreadm/s/spreadm.*$/spreadm 1000/' $OPAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 1000/' $OPAR
 $X $SIM
 cp ${R}.out ${R}_v3m9.out
 
@@ -85,103 +86,103 @@ cp ${R}.out ${R}_v3m9.out
 
 
 # only migration
-gsed -i '/spreadm/s/spreadm.*$/spreadm 0/' $OPAR
-gsed -i '/spreadv/s/spreadv.*$/spreadv 0/' $PAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 0/' $OPAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 0/' $PAR
 $X $SIM
 cp ${R}.out ${R}_v0m0.out
-gsed -i '/spreadv/s/spreadv.*$/spreadv 0.000001/' $PAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 0.000001/' $PAR
 $X $SIM
 cp ${R}.out ${R}_v1m0.out
-gsed -i '/spreadv/s/spreadv.*$/spreadv 0.00001/' $PAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 0.00001/' $PAR
 $X $SIM
 cp ${R}.out ${R}_v2m0.out
-gsed -i '/spreadv/s/spreadv.*$/spreadv 0.001/' $PAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 0.001/' $PAR
 $X $SIM
 cp ${R}.out ${R}_v3m0.out
-gsed -i '/spreadv/s/spreadv.*$/spreadv 0.00333/' $PAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 0.00333/' $PAR
 $X $SIM
 cp ${R}.out ${R}_v4m0.out
-gsed -i '/spreadv/s/spreadv.*$/spreadv 0.01/' $PAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 0.01/' $PAR
 $X $SIM
 cp ${R}.out ${R}_v5m0.out
-gsed -i '/spreadv/s/spreadv.*$/spreadv 0.033333/' $PAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 0.033333/' $PAR
 $X $SIM
 cp ${R}.out ${R}_v6m0.out
-gsed -i '/spreadv/s/spreadv.*$/spreadv 0.1/' $PAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 0.1/' $PAR
 $X $SIM
 cp ${R}.out ${R}_v7m0.out
-gsed -i '/spreadv/s/spreadv.*$/spreadv 0.333333/' $PAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 0.333333/' $PAR
 $X $SIM
 cp ${R}.out ${R}_v8m0.out
-gsed -i '/spreadv/s/spreadv.*$/spreadv 1.0/' $PAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 1.0/' $PAR
 $X $SIM
 cp ${R}.out ${R}_v9m0.out
 
 
 # only trade
-gsed -i '/spreadv/s/spreadv.*$/spreadv 0.000001/' $PAR
-gsed -i '/spreadm/s/spreadm.*$/spreadm 0/' $OPAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 0.000001/' $PAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 0/' $OPAR
 $X $SIM
 cp ${R}.out ${R}_v1m0.out
-gsed -i '/spreadm/s/spreadm.*$/spreadm 1000/' $OPAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 1000/' $OPAR
 $X $SIM
 cp ${R}.out ${R}_v1m1.out
-gsed -i '/spreadm/s/spreadm.*$/spreadm 10000/' $OPAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 10000/' $OPAR
 $X $SIM
 cp ${R}.out ${R}_v1m2.out
-gsed -i '/spreadm/s/spreadm.*$/spreadm 100000/' $OPAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 100000/' $OPAR
 $X $SIM
 cp ${R}.out ${R}_v1m3.out
-gsed -i '/spreadm/s/spreadm.*$/spreadm 333333/' $OPAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 333333/' $OPAR
 $X $SIM
 cp ${R}.out ${R}_v1m4.out
-gsed -i '/spreadm/s/spreadm.*$/spreadm 1000000/' $OPAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 1000000/' $OPAR
 $X $SIM
 cp ${R}.out ${R}_v1m5.out
-gsed -i '/spreadm/s/spreadm.*$/spreadm 3333333/' $OPAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 3333333/' $OPAR
 $X $SIM
 cp ${R}.out ${R}_v1m6.out
 
 
 
 # base simulation
-gsed -i '/spreadv/s/spreadv.*$/spreadv 0.002/' $PAR
-gsed -i '/spreadm/s/spreadm.*$/spreadm 100/' $OPAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 0.002/' $PAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 100/' $OPAR
 $X $SIM
 cp $R.out ${R}_base.out
 
 
 # relative importance of trade ad migration
-gsed -i '/spreadv/s/spreadv.*$/spreadv 0.002/' $PAR
-gsed -i '/spreadm/s/spreadm.*$/spreadm 100/' $OPAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 0.002/' $PAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 100/' $OPAR
 $X $SIM
 cp $R.out ${R}_t55.out
-gsed -i '/spreadv/s/spreadv.*$/spreadv 0.001/' $PAR
-gsed -i '/spreadm/s/spreadm.*$/spreadm 400/' $OPAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 0.001/' $PAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 400/' $OPAR
 $X $SIM
 cp $R.out ${R}_t46.out
-gsed -i '/spreadv/s/spreadv.*$/spreadv 0.0005/' $PAR
-gsed -i '/spreadm/s/spreadm.*$/spreadm 1600/' $OPAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 0.0005/' $PAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 1600/' $OPAR
 $X $SIM
 cp $R.out ${R}_t37.out
-gsed -i '/spreadv/s/spreadv.*$/spreadv 0.0002/' $PAR
-gsed -i '/spreadm/s/spreadm.*$/spreadm 100000/' $OPAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 0.0002/' $PAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 100000/' $OPAR
 $X $SIM
 cp $R.out ${R}_t28.out
-gsed -i '/spreadv/s/spreadv.*$/spreadv 0.004/' $PAR
-gsed -i '/spreadm/s/spreadm.*$/spreadm 25/' $OPAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 0.004/' $PAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 25/' $OPAR
 $X $SIM
 cp $R.out ${R}_t64.out
-gsed -i '/spreadv/s/spreadv.*$/spreadv 0.008/' $PAR
-gsed -i '/spreadm/s/spreadm.*$/spreadm 6/' $OPAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 0.008/' $PAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 6/' $OPAR
 $X $SIM
 cp $R.out ${R}_t73.out
-gsed -i '/spreadv/s/spreadv.*$/spreadv 0.016/' $PAR
-gsed -i '/spreadm/s/spreadm.*$/spreadm 1.6/' $OPAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 0.016/' $PAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 1.6/' $OPAR
 $X $SIM
 cp $R.out ${R}_t82.out
-gsed -i '/spreadv/s/spreadv.*$/spreadv 0.032/' $PAR
-gsed -i '/spreadm/s/spreadm.*$/spreadm 0.4/' $OPAR
+$SED -i '/spreadv/s/spreadv.*$/spreadv 0.032/' $PAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 0.4/' $OPAR
 $X $SIM
 cp $R.out ${R}_t91.out
 
@@ -189,20 +190,20 @@ cp $R.out ${R}_t91.out
 
 
 # Remove spread
-gsed -i '/LocalSpread/s/1/0/' $CTL
+$SED -i '/LocalSpread/s/1/0/' $CTL
 $X $SIM
 cp $R.out ${R}_nospread.out
-gsed -i '/LocalSpread/s/0/1/' $CTL
+$SED -i '/LocalSpread/s/0/1/' $CTL
 
 # Add climate disruptions
-gsed -i '/flucampl/s/0\.0/0.4/' $DAT
+$SED -i '/flucampl/s/0\.0/0.4/' $DAT
 $X $SIM
 cp $R.out ${R}_events.out
-gsed -i '/flucampl/s/0\.4/0.0/' $DAT
+$SED -i '/flucampl/s/0\.4/0.0/' $DAT
 
 
 # return to base setup
-gsed -i '/spreadv/s/spreadv.*$/spreadv 0.002/' $PAR
-gsed -i '/spreadm/s/spreadm.*$/spreadm 100/' $OPAR
-gsed -i '/LocalSpread/s/LocalSpread.*$/LocalSpread 1/' $CTL
-gsed -i '/flucampl/s/flucampl.*$/flucampl 0.0/' $DAT
+$SED -i '/spreadv/s/spreadv.*$/spreadv 0.002/' $PAR
+$SED -i '/spreadm/s/spreadm.*$/spreadm 100/' $OPAR
+$SED -i '/LocalSpread/s/LocalSpread.*$/LocalSpread 1/' $CTL
+$SED -i '/flucampl/s/flucampl.*$/flucampl 0.0/' $DAT
--- src/sisi/src/Version.cc
+++ src/sisi/src/Version.cc
@@ -5,4 +5,8 @@
 const char* Version::ProgramName    =  "SiSi";
 const char* Version::Number         =  "2.0";
 const char* Version::Program        =  "SiSi 2.0";
+<<<<<<< local
+const char* Version::CompletionTime =  "Okt 08 2009, 16:28:43 (uncompleted)";
+=======
 const char* Version::CompletionTime =  "Dec 18 2009, 14:53:05 (uncompleted)";
+>>>>>>> other
