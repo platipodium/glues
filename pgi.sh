c5d110b821940d5c7ffa2d6100aeafbd8e816f0a
Carsten Lemmen <carsten.lemmen@hzg.de>
2010-01-08 09:46:01 +0100

    Removed Version.cc which is automagically created from VERSION

--- pgi.sh
+++ pgi.sh
@@ -1,4 +1,6 @@
 ./bootstrap && ./configure CC=pgcc CXX=pgCC \
 --with-sisi-include=$HOME/opt/src/SiSi2.2/sisi/include \
 --with-sisi-lib=$HOME/opt/src/SiSi2.2/sisi/lib-pgi \
-&& (cd src ; gmake -k clean ) &&  gmake -k -j4 && ./run.sh
+&& (cd src ; gmake -k clean ) 
+gmake -k -j4 || (cd src gmake -k j4)
+ ./run.sh
--- src/sisi/src/Version.cc
+++
@@ -1,8 +0,0 @@
-// Generated automatically from VERSION by 'make version'
-
-#include "Version.hh"
-
-const char* Version::ProgramName    =  "SiSi";
-const char* Version::Number         =  "2.0";
-const char* Version::Program        =  "SiSi 2.0";
-const char* Version::CompletionTime =  "Jan 07 2010, 17:27:02 (uncompleted)";
