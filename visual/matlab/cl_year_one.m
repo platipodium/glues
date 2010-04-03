dcfc7c16e0aa7534ae65cb5fc50d2a6db9d73a5e
Carsten Lemmen <carsten.lemmen@gkss.de>
2010-04-03 22:14:35 +0200

    hlisation
    :q
    :q!
    :q!
    
    
    
    
    g: bitte gib eine versions-meldung ein. zeilen beginnend mit 'hg:' werden entfernt.
    hg: leere versionsmeldung wird das ?bernehmen abbrechen.

---
+++ visual/matlab/cl_year_one.m
@@ -0,0 +1,17 @@
+function cl_year_one(ax)
+% Corrects a zero year on an x axis to 1 AD
+
+xt=get(ax,'XTick');
+i0=find(xt==0);
+if ~isempty(i0)
+  xtl=get(ax,'XTickLabel');
+  len=length(xtl(i0,:));
+  xtl(i0,1:len)=' ';
+  if len<4 xtl(i0,1)='1';
+  else xtl(i0,1:4)='1 AD';
+  end
+  xt(i0)=1;
+  set(gca,'XTick',xt,'XTickLabel',xtl);
+end
+
+end
