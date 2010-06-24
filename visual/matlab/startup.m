01b883a24d2e9b713113aa4910f599af1d779c96
Carsten Lemmen <carsten.lemmen@hzg.de>
2010-06-24 17:24:32 +0200

    Minor corrections

--- visual/matlab/startup.m
+++ visual/matlab/startup.m
@@ -1,14 +1,5 @@
 %
-newpath=pwd;
+
+newpath='/h/lemmen/devel/glues/visual/matlab';
 addpath(genpath(newpath));
 fprintf('Added %s and subdirectories to MATLAB search path\n',newpath);
-
-newpath=fullfile(getenv('HOME'),'matlab','m_map');
-if exist(newpath,'file')
-  addpath(newpath);
-  fprintf('Added %s to MATLAB search path\n',newpath);
-end
-
-if ~exist('m_proj')
-  warning('M_PROJ mapping toolbox not found, please install with command ''get_mproj''');
-end
