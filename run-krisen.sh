14031dfced54768e34c4e392d718e5bdf08956bf
Carsten Lemmen <carsten.lemmen@hzg.de>
2010-07-15 22:00:35 +0200

    Added new run skript and reverted back to including events in default simulation

---
+++ run-krisen.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# this script runs the scenarios for the Umweltkrisen paper,
+# corresponding routines for visualisation is do_krisen.m
+
+SED=gsed
+X=src/glues
+P=examples/simulations/685/lbk
+S=examples/setup/685
+
+SIM=$P.sim
+CTL=$P.ctl
+SCE=$P.sce
+DAT=$P.dat
+OPAR=$P.opar
+R=$S/results
+PAR=$P.par
+T=test
+
+# base simulation with events
+$SED -i '/flucampl/s/flucampl.*$/flucampl 0.4/' $DAT
+$SED -i '/float  gammab/s/gammab.*$/gammab 0.0040/' $PAR
+
+$X $SIM
+cp $R.out ${R}_base.out
+cp $T.nc krisen_base.nc
+
+# Add climate disruptions
+$SED -i '/flucampl/s/0\.4/0.0/' $DAT
+$SED -i '/float gammab/s/0\.0040/0.0037195/' $PAR 
+$X $SIM
+cp $R.out ${R}_nofluc.out
+cp $T.nc krisen_nofluc.nc
+
+# return to base setup
+$SED -i '/flucampl/s/flucampl.*$/flucampl 0.4/' $DAT
+$SED -i '/float  gammab/s/gammab.*$/gammab 0.0040/' $PAR
