812d1f3c0ec7288605b4af0e6d87fddec3c66517
Carsten Lemmen <carsten.lemmen@hzg.de>
2010-07-27 13:33:55 +0200

    Added missing Version.cc

---
+++ src/sisi/src/Version.cc
@@ -0,0 +1,8 @@
+// Generated automatically from VERSION by 'make version'
+
+#include "Version.hh"
+
+const char* Version::ProgramName    =  "SiSi";
+const char* Version::Number         =  "2.0";
+const char* Version::Program        =  "SiSi 2.0";
+const char* Version::CompletionTime =  "Jan 11 2010, 16:04:16 (uncompleted)";
