86513410a55adc50fd003b5a2fd5cb70b658c232
Carsten Lemmen <carsten.lemmen@gkss.de>
2010-07-27 14:19:42 +0200

    updated to 1.1.12

--- src/Messages.cc
+++ src/Messages.cc
@@ -43,7 +43,7 @@ namespace glues {
       << "*                                                              *" << endl
       << "*  Authors Carsten Lemmen & Kai Wirtz                          *" << endl
       << "*  Contact <carsten.lemmen@gkss.de>                            *" << endl 
-      << "*  Last change  2009-08-14 / Version 1.1.11                     *" << endl
+      << "*  Last change  2009-08-14 / Version 1.1.12                     *" << endl
       << "****************************************************************" << endl
       << endl;
     return;
@@ -53,7 +53,7 @@ namespace glues {
     cout 
       << endl
       << "****************************************************************" << endl
-      << "*                Thank you for using GLUES 1.1.11" << endl
+      << "*                Thank you for using GLUES 1.1.12" << endl
       << "****************************************************************" << endl
       << endl;
     return;
