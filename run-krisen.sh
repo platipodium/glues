84aff1aab608fa5a7254b30e769d12807df674cb
Carsten Lemmen <carsten.lemmen@gkss.de>
2010-07-30 15:49:29 +0200

    Added scenario for increased KnowledgeLoss

--- run-krisen.sh
+++ run-krisen.sh
@@ -17,21 +17,32 @@ R=$S/results
 PAR=$P.par
 T=test
 
-# base simulation with events
+# base simulation with events and slight knowledge loss
 $SED -i '/flucampl/s/flucampl.*$/flucampl 0.4/' $DAT
 $SED -i '/float.*gammab/s/gammab.*$/gammab 0.0040/' $PAR
-
+$SED -i '/float.*KnowledgeLoss/s/KnowledgeLoss.*$/KnowledgeLoss 0.3/' $PAR
 $X $SIM
 cp $R.out ${R}_base.out
 cp $T.nc krisen_base.nc
 
-# Remove climate disruptions
+# Add larger knowledge loss
 $SED -i '/flucampl/s/0\.4/0.0/' $DAT
 $SED -i '/float.*gammab/s/0\.0040/0.0037195/' $PAR 
+$SED -i '/float.*KnowledgeLoss/s/0\.3/2.0/' $PAR
+$X $SIM
+cp $R.out ${R}_loss.out
+cp $T.nc krisen_loss.nc
+
+
+# Remove climate disruptions and knowledge loss
+$SED -i '/flucampl/s/0\.4/0.0/' $DAT
+$SED -i '/float.*gammab/s/0\.0040/0.0037195/' $PAR 
+$SED -i '/float.*KnowledgeLoss/s/2.0/0.0/' $PAR
 $X $SIM
 cp $R.out ${R}_nofluc.out
 cp $T.nc krisen_nofluc.nc
 
 # return to base setup
 $SED -i '/flucampl/s/flucampl.*$/flucampl 0.4/' $DAT
-$SED -i '/float gammab/s/gammab.*$/gammab 0.0040/' $PAR
+$SED -i '/float.*gammab/s/gammab.*$/gammab 0.0040/' $PAR
+$SED -i '/float.*KnowledgeLoss/s/KnowledgeLoss.*$/KnowledgeLoss 0.3/' $PAR
