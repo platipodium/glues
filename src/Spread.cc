20f7637c6373a6699d1399de12dfecb93f4d9337
Carsten Lemmen <carsten.lemmen@gkss.de>
2010-10-15 11:09:12 +0200

    Added diagnostics to stderr, not applied yet

--- src/Spread.cc
+++ src/Spread.cc
@@ -91,6 +91,7 @@ double calc_spread_single(unsigned int i) {
 	//double maxforce;
   double jtech,itech,dp0,dp1,indom,ndx,iqfarm,igerm,iresist;
   double iinfl,jinfl;
+  unsigned int import_id, export_id;
   GeographicalNeighbour* neigh;
   
   /* ------------------------------------------------- */
@@ -149,13 +150,8 @@ double calc_spread_single(unsigned int i) {
       ijpop=(iinfl*iarea+jinfl*jarea)/(iarea+jarea);
       force=exch*(ijpop-iinfl);
 
-      /** Alternative formulation * /
-      
-      if ( (iid==270) && (irgr<0) && (irgr<jrgr) ) {
-	fprintf(stdout,"%3d: %4.0f %4.0f %3.0f | %3d: %4.0f %4.0f %3.0f | I=%3.0f J=%3.0f IJ=%3.0f F=%8.2f\n",iid,ipop*1000.0,iarea/1000.0,itech*100.0,jid,jpop*1000.0,jarea/1000.0,jtech*100.0,iinfl*100.0,jinfl*100.0,ijpop*100.0,force*1000.0);
-      */
-	
-      /** Calculated maximum relative change in both
+      /** Alternative formulation 
+       Calculated maximum relative change in both
 	  target and source regions
 	  introduced 2008-01-22 cl */
 
@@ -165,31 +161,33 @@ double calc_spread_single(unsigned int i) {
 	   *jpop*jarea/ipop/iarea);
       
 	   dp0=min(force,maxforce/iarea);*/ 
-      dp0=force;
-      dp1=-iarea*dp0/jarea;
+    dp0=force;
+    dp1=-iarea*dp0/jarea;
       
+    sprd_p[i*N_POPVARS+4]+=dp0*ipop;
+    sprd_p[j*N_POPVARS+4]+=dp1*jpop;
     sprd[i*N_POPVARS+4]+=dp0*ipop;
     sprd[j*N_POPVARS+4]+=dp1*jpop;
     
-     /**
-	 For unidirectional transport of traits and traits in people,
-	 identify source and target. Change occurs in
-	 importing region only with traits of exporter 
-	       */
-	       
-
-    int import_id=0,export_id=0;
-    double import_change=0;
+    /**
+    For unidirectional transport of traits and traits in people,
+    identify source and target. Change occurs in
+    importing region only with traits of exporter 
+	*/     
+    double import_change;
+    double export_change;
     
     if(force<0) {  // outward pressure
       export_id=i;
       import_id=j;
       import_change=dp1;
+      export_change=dp0;
     }
     else { // inward pressure
       export_id=j;
       import_id=i;
       import_change=dp0;
+      export_change=dp1;
     }
     
     double export_tech  =populations[export_id].Technology();
@@ -202,58 +200,53 @@ double calc_spread_single(unsigned int i) {
     double import_pop   =populations[import_id].Density();
     double import_tech  =populations[import_id].Technology();   
     
+    assert(import_change>=0);
+    assert(export_change<=0);
+    assert(import_pop>0);
+    assert(export_pop>0);
+    
     double import_tech_p=import_change>0?(export_tech*export_pop/import_pop)*import_change:0;
     double import_ndom_p=import_change>0?(export_ndom*export_pop/import_pop)*import_change:0;
     double import_qfarm_p=import_change>0?(export_qfarm*export_pop/import_pop)*import_change:0;
     double import_tech_i=import_change>0?traitspread(export_tech,import_tech,import_change):0;
     double import_ndom_i=import_change>0?traitspread(export_ndom,populations[import_id].Ndomesticated(),import_change):0;
     double import_pop_m=import_change*import_pop;
+    double export_pop_m=export_change*export_pop;
     
-    /** TODO pop can be negative */
-    assert(import_pop>0);
-    assert(import_change>=0);
     if (import_change>0) {
    
       /** 
       Spread of traits with people 
       This has not been documented in WL03
+      In sprd_p, all exchanges with all neighbours are summed up.
+      @todo do the same for germs/resist variables (index 5,6)
       */
-   
-      sprd[import_id*N_POPVARS+0] += (export_tech*export_pop/import_pop)*import_change;
-      sprd[import_id*N_POPVARS+1] += (export_ndom*export_pop/import_pop)*import_change;
-      sprd[import_id*N_POPVARS+2] += (export_qfarm*export_pop/import_pop)*import_change;
-      
-      sprd_p[import_id*N_POPVARS+0] += (export_tech*export_pop/import_pop)*import_change;
-      sprd_p[import_id*N_POPVARS+1] += (export_ndom*export_pop/import_pop)*import_change;
-      sprd_p[import_id*N_POPVARS+2] += (export_qfarm*export_pop/import_pop)*import_change;
-      
+      sprd_p[import_id*N_POPVARS+0] += import_tech_p;
+      sprd_p[import_id*N_POPVARS+1] += import_ndom_p;
+      sprd_p[import_id*N_POPVARS+2] += import_qfarm_p;
+      sprd[import_id*N_POPVARS+0] += import_tech_p;
+      sprd[import_id*N_POPVARS+1] += import_ndom_p;
+      sprd[import_id*N_POPVARS+2] += import_qfarm_p;
+
+	  /**
+	  Spread of traits by information/trade
+	  This is documented in WL03
+	  In sprd_i, all exchanges with all neighbours are summed up.
+	  @param controlled by parameter spreadm (in function traitspread)
+	  @todo This exchange may apply (or not) to qfarm and germs
+	  @todo This exchange may apply (or not) to resist with function genospread
+      */
+      sprd_i[import_id*N_POPVARS+0]+=import_tech_i;
+      sprd_i[import_id*N_POPVARS+1]+=import_ndom_i;
+      sprd[import_id*N_POPVARS+0]+=import_tech_i;
+      sprd[import_id*N_POPVARS+1]+=import_ndom_i;
+      //sprd_i[import_id*N_POPVARS+5]+=traitspread(igerm,populations[j].Germs(),import_change);
+      //sprd_i[import_id*N_POPVARS+3]+=genospread(iresist,populations[j].Resist(),export_tech);
+           
       //spreadmatrix[numberOfRegions*(import_id*N_POPVARS+2)*j] = (export_qfarm*export_pop/import_pop)*import_change;
       
-      /* TODO: germs and resist
-      sprd[import_id*N_POPVARS+3] += (export_resist*export_pop/import_pop)*import_change;
-      sprd[import_id*N_POPVARS+5] += (export_germ*export_pop/import_pop)*import_change;
-      */
-  
-      /*-------------------------------------------------------*/
-      /*   Spread of traits with trade (see parameter spreadm)   */
-      /*-------------------------------------------------------*/
-      //    printf("\t do spread\t%d\t%ld\n",iid,sprd);
-
-     sprd[import_id*N_POPVARS+0]+=traitspread(export_tech,import_tech,import_change);
-     sprd[import_id*N_POPVARS+1]+=traitspread(export_ndom,populations[import_id].Ndomesticated(),import_change);
-       
-     sprd_i[import_id*N_POPVARS+0]+=traitspread(export_tech,import_tech,import_change);
-     sprd_i[import_id*N_POPVARS+1]+=traitspread(export_ndom,populations[import_id].Ndomesticated(),import_change);
-       
-      // Qfarming should not spread, thus next line commented
-      //sprd[i1*N_POPVARS+2]+=traitspread(iqfarm,populations[j].Qfarming());
-
-// TODO germs and resist
-      //sprd[import_id*N_POPVARS+5]+=traitspread(igerm,populations[j].Germs(),import_change);
-      //sprd[import_id*N_POPVARS+3]+=genospread(iresist,populations[j].Resist(),export_tech);
-
     
-  /* cerr << import_id << " " populations[import_id] << " rgr= " << populations[import_id].RelativeGrowthrate() 
+      /* cerr << import_id << " " populations[import_id] << " rgr= " << populations[import_id].RelativeGrowthrate() 
     	<< "/ import_change = " << import_change << endl
     	<< force << " / " << dp0 << " / " << dp1  << endl
     	<< ijpop << " / " << iinfl << " / " << jinfl << " / " << exch << endl 
@@ -262,37 +255,38 @@ double calc_spread_single(unsigned int i) {
 
   
       double ch;
-      ch=TimeStep*sprd[i*N_POPVARS+4];
+      ch=TimeStep*sprd_p[i*N_POPVARS+4];
       if(fabs(ch/(ipop+0.01))>RelChange)
-	cout<<"HighDeni "<< state_names[4] <<" "<<iid<<"\t: "<< ipop
-	<<"+"<<ch<<endl;
-      ch=TimeStep*sprd[j*N_POPVARS+4];
+	    cout<<"HighDeni "<< state_names[4] <<" "<<iid<<"\t: "<< ipop
+	      <<"+"<<ch<<endl;
+      ch=TimeStep*sprd_p[j*N_POPVARS+4];
       if(fabs(ch/(jpop+0.01))>RelChange)
-	cout<<"HighDenj "<< state_names[4] <<" "<<jid<<"\t: "<< jpop
-	<<"+"<<ch<<endl;
+	    cout<<"HighDenj "<< state_names[4] <<" "<<jid<<"\t: "<< jpop
+	 <<"+"<<ch<<endl;
       
-  if (0& (iid==211 || iid==271 || iid== 235))
-     printf("%d %d\t%1.1f %1.1f\t%1.1f \n",iid,jid,dp0*1E3,dp1*1E3,ndx*1E3);
-
       /** For diagnostic output calculate total migration activity */
       sprdm[i]+=fabs(dp0*ipop);
       sprdm[j]+=fabs(dp1*jpop);
-      }
+    }
 
-   cerr << import_id << " " /*populations[import_id] /*<< " rgr= " << populations[import_id].RelativeGrowthrate() 
-    	<< "/ import_change = " << import_change << endl
-    	<< force << " / " << dp0 << " / " << dp1  << endl
-    	<< ijpop << " / " << iinfl << " / " << jinfl << " / " << exch << endl */
-    	<< export_id // populations[export_id] 
-    	<< " dT " << sprd_p[import_id*N_POPVARS+0] << " " <<  sprd_i[import_id*N_POPVARS+0]
-    	<< " dN " << sprd_p[import_id*N_POPVARS+1] << " " <<  sprd_i[import_id*N_POPVARS+1]
-    	<< " dQ " << sprd_p[import_id*N_POPVARS+2] << " " <<  sprd_i[import_id*N_POPVARS+2]
-    	<< " dP " << sprd_p[import_id*N_POPVARS+3] << " " <<  sprd_i[import_id*N_POPVARS+3] 	
+/*    cerr << import_id << " " << export_id 
+    	<< " dT " << sprd_p[import_id*N_POPVARS+0] << " " <<  sprd_i[import_id*N_POPVARS+0] << " " <<  sprd_i[import_id*N_POPVARS+0]
+    	<< " dN " << sprd_p[import_id*N_POPVARS+1] << " " <<  sprd_i[import_id*N_POPVARS+1] << " " <<  sprd_i[import_id*N_POPVARS+1]
+    	<< " dQ " << sprd_p[import_id*N_POPVARS+2] << " " <<  sprd_i[import_id*N_POPVARS+2] << " " <<  sprd_i[import_id*N_POPVARS+2]
+    	<< " dP " << sprd_p[import_id*N_POPVARS+3] << " " <<  sprd_i[import_id*N_POPVARS+3] << " " <<  sprd_i[import_id*N_POPVARS+4]	
     	<< endl;
-    	
+    //*/	
     }
     neigh = neigh->Next();
   }
+  
+  /** Collect all information in matrix sprd 
+    @todo: commented since results do not agree with calculation of sprd above 
+  */
+  for (unsigned int ivar=0; ivar<5; ivar++) {
+    //sprd[import_id*N_POPVARS+ivar] = sprd_p[import_id*N_POPVARS+ivar] + sprd_i[import_id*N_POPVARS+ivar]; 
+  }
+  
   return fabs(sprd[i*N_POPVARS+4])*iarea;
 }
 
