d017cab90399a263cab424bd3d40d2d9cd9cf2a4
Carsten Lemmen <carsten.lemmen@hzg.de>
2010-10-19 22:35:30 +0200

    Code revision by Caleb T

--- src/AsciiTable.cc
+++ src/AsciiTable.cc
@@ -2,53 +2,106 @@
     @date 2007-05-25
 */
 
-#include "AsciiTable.h"
+/*
+Caleb T.
+10/19/2010
 
-int AsciiTable::getRow(std::string& str) {
+Modification to line in which for() loop test condition
+was placed improperly.
+
+Signed/unsigned mismatch 
+"if (field.size() < (int)ncol)"
+size_t is a typedef which can change from 32 to 64 bit
+platforms, and is returned by .size() for vectors and strings, etc.
+
+Other minor modifications also, I think this will work as expected.
+*/
+#include "AsciiTable.h"
+#include <sstream>
+#include <algorithm>
+
+bool AsciiTable::getRow(std::string& str) {
   char ch;
-  row="";
-  
-  while(instream.get(ch) && !isEol(ch)) { row+=ch; };
+  row.clear();
+
+  while(instream.get(ch))
+  {
+	  if( !isEol(ch) )
+		  row.push_back(ch);
+	  else
+		  break;
+  }
   splitRow();
   str=row;
   return !instream.eof();
 }
 
-int AsciiTable::isEol(char ch) {
-  int eol;
+bool AsciiTable::isEol(char ch) {
   
-  eol = ( ch=='\n' || ch=='\r' );
-  if (eol) {
-    instream.get(ch);
-    if (!instream.eof() && ch!='\n' && ch!='\r') instream.putback(ch);
-  }
-  return eol;
+	bool eol;
+	eol = ( ch=='\n' || ch=='\r' );
+
+	if (eol)
+	{
+		//Verify next character(s) also not eol.
+		while( instream.good() )
+		{
+			instream.get(ch);
+			if (!instream.eof() && ch!='\n' && ch!='\r')
+			{
+				//ch is not eol, put back.
+				instream.putback(ch);
+				break;
+			}
+		}
+	}
+	return eol;
 }
 
-int AsciiTable::splitRow() {
-  string entry;
-  unsigned int i,j;
+size_t AsciiTable::splitRow() {
+  std::string entry;
+  size_t i,j;
 
   ncol=0;
-  if (row.length() == 0) return 0;
+  if (row.size() == 0) return 0;
   
-  for (i=0,j=0; ; j<row.length()) {
-    
-    while (j <= i+1) j=row.find_first_of(separator,j);
-    entry=row.substr(i,j);
-    ncol++;
-    
-    if (field.size() < (int)ncol) field[ncol]=entry;
-    else field.push_back(entry);
-    
-    j=j+1;
+  //Updated.
+  j = i = 0;
+  while( i < row.size() && j < row.size() )
+  {
+	  //Replace separator with whitespace for
+	  //extraction with the extraction operator.
+	  //Hopefully this will be more robust.
+	  j = row.find( separator, i );
+	  if( j == std::string::npos )
+		  break;
+	  std::fill_n( row.begin()+j, separator.size(), ' ' );
+	  i=j+1;
   }
-  
-	// TODO: pgi complaines about unreachable statement here
+
+  std::stringstream ss(row);
+  while( ss >> entry )
+  {
+	  //Extract entries & insert into vector<string> field.
+	  ncol++;
+	  if( field.size() >= ncol)
+		  field.at(ncol-1) = entry;
+	  else
+		  field.push_back( entry );
+  }
+
   return ncol;
 }
 
-string AsciiTable::getField(int icol) {
-  if (icol<0 || icol>ncol) return "";
-  else return field[icol];
+std::string AsciiTable::getField(size_t icol)
+{
+	//icol is unsigned.
+	if( (ncol == 0) || (icol > (ncol-1)) )
+	{
+		return "";
+	}
+	else
+	{
+		return field.at(icol);
+	}
 }
--- src/AsciiTable.h
+++ src/AsciiTable.h
@@ -2,31 +2,44 @@
     @date 2007-05-25
 */
 
+/* Caleb T.
+Changed some types,
+avoided signed/unsigned bugs.
+
+Added bool return types where int
+is used as a boolean value.
+
+Fixed bug where calling getField(#)
+will subscript a vector out of range
+due to not initializing ncol to 0 in the
+constructor.
+*/
+
 #ifndef __asciitable_h__
 #define __asciitable_h__
 
 #include <iostream>
 #include <string>
 #include <vector>
-using namespace std;
+#include <cstddef>
 
-class AsciiTable{
+class AsciiTable {
 
  private:
-    istream& instream;
-    string separator,row;
-    vector<std::string> field;
-    int ncol;
+    std::istream &instream;
+    std::string separator,row;
+    std::vector<std::string> field;
+    size_t ncol;
 
-    int splitRow();
-    int isEol(char);    
+    size_t splitRow();
+    bool isEol(char);
   
  public:
-    AsciiTable(istream& in=cin,string sep=" ") : instream(in),separator(sep) {}
+    AsciiTable(std::istream& in=std::cin, std::string sep=" ") : instream(in),separator(sep),ncol(0) {}
 
-    int getNCols() const { return ncol; }
-    int getRow(std::string&);
-    string getField(int); 
+    size_t getNCols() const { return ncol; }
+    bool getRow(std::string&);
+    std::string getField(size_t);
 
 };
 
