ab355edf5c3afa5318e884ace1b386b159ee12c1
Carsten Lemmen <carsten.lemmen@hzg.de>
2010-10-26 11:37:55 +0200

    Cosemtic changes to IO.h

--- patch.sh
+++ patch.sh
@@ -2,7 +2,7 @@
 # Patch current bugs
 
 # 1. Patch dynamic lib of sisi to static lib
-for F in src/Makefile src/region/Makefile src/variation/Makefile ; do
-sed -i '/LIBS =/s#-lSiSi#'`pwd`'/src/sisi/lib/.libs/libSiSi.a#' $F; 
-done
+#for F in src/Makefile src/region/Makefile src/variation/Makefile ; do
+sed -i '/LIBS =/s#-lSiSi#src/sisi/lib/.libs/libSiSi.a#' src/Makefile; 
+#done
 
