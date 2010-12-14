f2f25eadef474ab7c23f41471a3e9a7cd51ed251
Carsten Lemmen <carsten.lemmen@hzg.de>
2010-12-14 15:43:42 +0100

    Figures 1,3 done, working on #4 (ammerman plot)

--- visual/matlab/do_eurolbk.m
+++ visual/matlab/do_eurolbk.m
@@ -47,24 +47,76 @@ slat=Forenbaher.Latitude;
 slon=Forenbaher.Long;
 period=Forenbaher.Period;
 site=Forenbaher.Site_name;
-age=Forenbaher.Median_age;
-ns=length(age);
+sage=Forenbaher.Median_age;
+ns=length(sage);
 dlat=[slat' repmat(lat(272),ns,1)];
 dlat=reshape(dlat',2*ns,1);
 dlon=[slon' repmat(lon(272),ns,1)];
 dlon=reshape(dlon',2*ns,1);
-dists=m_lldist(dlon,dlat);
+sdists=m_lldist(dlon,dlat);
+sdists=sdists(1:2:end);
 
+[ifound,nfound,lonlim,latlim]=find_region_numbers(reg);
 
-%% plot farming timing
-clp_nc_variable('var','farming','threshold',0.5,'reg',reg,'marble',2,'transparency',1,'nocolor',0,...
-      'showstat',0,'timelim',timelim,'showtime',0,'flip',1,'showvalue',1,...
-      'file','../../eurolbk_base.nc','figoffset',0,'sce','base');
+ncol=19;
+cmap=flipud(jet(ncol));
+stime=1950-sage;
+iscol=floor((stime-min(timelim))./(timelim(2)-timelim(1))*ncol)+1;
+viscol=find(iscol>0 & iscol<=ncol & slat>=latlim(1) & slat<=latlim(2) ...
+    & slon>=lonlim(1) & slon<=lonlim(2));
   
 
-return
 
-%% Plot region network
+%% Do Ammerman plot (figure 4)
+figure(1); clf reset; hold on;
+plot(-stime,sdists,'bo');
+ylabel('Distance from Levante');
+xlabel('Time (year BC)'); 
+set(gca,'Xlim',-fliplr([timelim]));
+set(gca,'XDir','reverse');
+plot(8000:-200:4000,0:200:4000,'k--');
+
+dlat=[lat' repmat(lat(272),nreg,1)];
+dlat=reshape(lat',2*nreg,1);
+dlon=[lon' repmat(lon(272),nreg,1)];
+dlon=reshape(dlon',2*nreg,1);
+dists=m_lldist(dlon,dlat);
+dists=dists(1:2:end);
+
+
+%% plot farming timing (figure 3)
+ncol=19;
+[data,basename]=clp_nc_variable('var','farming','threshold',0.5,'reg',reg,'marble',2,'transparency',1,'nocolor',0,...
+      'showstat',0,'timelim',timelim,'showtime',0,'flip',1,'showvalue',0,...
+      'file','../../eurolbk_base.nc','figoffset',0,'sce','base','noprint',1,'ncol',ncol);
+  
+cb=findobj('tag','colorbar');
+cmap=flipud(jet(ncol));
+cmap=rgb2hsv(cmap);
+cmap(:,2)=cmap(:,2)*0.5;
+cmap=hsv2rgb(cmap);
+colormap(cmap);
+ytl=get(cb,'YTickLabel');
+ytl=ytl(:,2:end);
+set(cb,'YTickLabel',ytl);
+ytt=get(cb,'Title');
+set(ytt,'String','Year BC');
+
+for i=1:length(viscol)
+  m_plot(slon(viscol(i)),slat(viscol(i)),'ko','MarkerFaceColor',cmap(iscol(viscol(i)),:));
+end
+
+
+for ir=1:-nhreg  
+  m_text(lon(hreg(ir)+1),lat(hreg(ir)+1),letters(ir),'background','w',...
+      'Horizontal','center','Vertical','middle');
+end
+m_coast('color','k');
+m_grid('box','fancy','linestyle','none');
+plot_multi_format(gcf,basename);
+
+  
+%% Plot region network (figure 1)
 [data,basename]=clp_nc_neighbour('reg',reg,'marble',2,'transparency',1,'nocolor',0,...
       'showstat',0,'showtime',0,'fontsize',10,'showregion',0,...
       'file','../../eurolbk_base.nc','figoffset',0,'sce','base','noprint',1);
@@ -73,6 +125,9 @@ for ir=1:nhreg
   m_text(lon(hreg(ir)+1),lat(hreg(ir)+1),letters(ir),'background','y',...
       'Horizontal','center','Vertical','middle');
 end
+m_coast('color','k','linestyle','none');
+m_grid('box','fancy');
+
 plot_multi_format(gcf,basename);
 
 
