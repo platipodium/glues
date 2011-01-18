36b24d93fcd018c7f3d241c32afd1b75368696a7
Carsten Lemmen <carsten.lemmen@hzg.de>
2011-01-18 11:59:17 +0100

    Added routine for gridded output.  TODO: compare with clp_nc_pangaea

---
+++ visual/matlab/cl_glues2grid.m
@@ -0,0 +1,257 @@
+function cl_glues2grid(varargin)
+% This function takes glues results for glues regions, and maps them to a 
+% grid of specified resolution
+
+arguments = {...
+  {'timelim',[-9500,-9000]},...
+  {'variables',{'population_density','technology'}},...
+  {'timestep',100},...
+  {'file','../../test.nc'},...
+  {'resolution',0.5}
+};
+
+cl_register_function;
+
+[a,rargs]=clp_arguments(varargin,arguments);
+for i=1:a.length 
+  lv=length(a.value{i});
+  if (lv>1 & iscell(a.value{i}))
+    for j=1:lv
+      eval( [a.name{i} '{' num2str(j) '} = ''' char(clp_valuestring(a.value{i}(j))) ''';']);         
+    end
+  else
+    eval([a.name{i} '=' clp_valuestring(a.value{i}) ';']); 
+  end
+end
+
+
+%% Read results file
+if ~exist(file,'file') error('File does not exist'); end
+ncid=netcdf.open(file,'NC_NOWRITE');
+
+varname='region'; varid=netcdf.inqVarID(ncid,varname);
+[varname,xtype,dimids,natts]=netcdf.inqVar(ncid,varid);
+rdim=dimids;
+id=netcdf.getVar(ncid,varid);
+nreg=length(id);
+varname='time'; varid=netcdf.inqVarID(ncid,varname);
+time=netcdf.getVar(ncid,varid);
+
+[ndim nvar natt udimid] = netcdf.inq(ncid); 
+if ~iscell(variables) 
+    
+  if strcmp(variables,'all')
+    clear('variables');
+    for varid=0:nvar-1
+      [varname,xtype,dimids,natts]=netcdf.inqVar(ncid,varid);
+      variables{varid+1}=varname;
+    end
+  else
+      v=variables; clear('variables');
+      variables{1}=v;
+  end
+end
+
+% Find time dimension
+tid=netcdf.inqVarID(ncid,netcdf.inqDim(ncid,udimid));
+tdim=udimid;
+time=netcdf.getVar(ncid,tid);
+ntime=length(time);
+if ntime>1
+  dtime=abs(time(2)-time(1));
+  itime=find(time>=timelim(1) & time<=timelim(2));
+  if isempty(itime)
+    error('Not data in specified time range')
+  end
+  itime=itime([1:round(timestep/dtime):length(itime)]);
+else itime=1;  
+end
+time=time(itime);
+
+
+ofile=strrep(file,'.nc',['_' num2str(resolution) 'x' num2str(resolution) '.nc']);
+if exist(ofile,'file') delete(ofile); end
+ncout=netcdf.create(ofile,'NOCLOBBER');
+
+latgrid=[90-resolution/2.0:-resolution:-90+resolution/2.0];
+longrid=[-180+resolution/2.0:resolution:180-resolution/2.0];
+nglon=length(longrid);
+nglat=length(latgrid);
+
+timedimid=netcdf.defDim(ncout,'time',0);
+londimid=netcdf.defDim(ncout,'lon',nglon);
+latdimid=netcdf.defDim(ncout,'lat',nglat);
+lonvarid=netcdf.defVar(ncout,'time','NC_DOUBLE',timedimid);
+lonvarid=netcdf.defVar(ncout,'lon','NC_DOUBLE',londimid);
+latvarid=netcdf.defVar(ncout,'lat','NC_DOUBLE',latdimid);
+regvarid=netcdf.defVar(ncout,'region','NC_INT',[londimid,latdimid]);
+
+% Copy attributes for GLOBAL, time, region
+varids=[netcdf.getConstant('NC_GLOBAL') netcdf.inqVarID(ncid,'time') netcdf.inqVarID(ncid,'region')];
+ovarids=[netcdf.getConstant('NC_GLOBAL') netcdf.inqVarID(ncout,'time') netcdf.inqVarID(ncout,'region')];
+for ivarid=1:length(varids)
+  varid=varids(ivarid);
+  ovarid=ovarids(ivarid);
+  if ivarid>1 [name,xtype,dimids,natts]=netcdf.inqVar(ncid,varid); else natts=natt; end
+  for iatt=0:natts-1
+    attname = netcdf.inqAttName(ncid,varid,iatt);
+    netcdf.copyAtt(ncid,varid,attname,ncout,ovarid);
+  end
+  netcdf.putAtt(ncout,ovarid,'date_of_modification',datestr(now));
+end
+
+% Make attributes for lat, lon
+varid=netcdf.inqVarID(ncout,'lat');
+netcdf.putAtt(ncout,varid,'units','degrees_north');
+netcdf.putAtt(ncout,varid,'long_name','latitude');
+netcdf.putAtt(ncout,varid,'description','centered latitude of grid cell');
+netcdf.putAtt(ncout,varid,'coordinates','lat');
+netcdf.putAtt(ncout,varid,'date_of_creation',datestr(now));
+varid=netcdf.inqVarID(ncout,'lon');
+netcdf.putAtt(ncout,varid,'units','degrees_east');
+netcdf.putAtt(ncout,varid,'long_name','longitude');
+netcdf.putAtt(ncout,varid,'description','centered longitude of grid cell');
+netcdf.putAtt(ncout,varid,'coordinates','lon');
+netcdf.putAtt(ncout,varid,'date_of_creation',datestr(now));
+
+
+stype=netcdf.getConstant('NC_FLOAT');
+dtype=netcdf.getConstant('NC_DOUBLE');
+itype=netcdf.getConstant('NC_INT');
+
+
+% Define variables and copy atts
+for ivar=1:length(variables)
+  varname=variables{ivar};
+  switch varname
+    case {'time','region','latitude','longitude','area'}, continue;
+  end
+  
+  % Exclude instantaneous variables 
+  if (timestep~=dtime)
+    switch varname
+      case {'relative_growth_rate','migration_density'}, continue;
+    end
+    if findstr('spread',varname) continue; end
+  end
+  
+  try 
+    varid=netcdf.inqVarID(ncid,varname);
+  catch
+    warning('Requested variable %s not in input file',varname);
+    continue;
+  end
+  [varname,xtype,dimids,natts]=netcdf.inqVar(ncid,varid);  
+  if length(dimids)==1
+    if dimids==tdim
+      ovarid=netcdf.defVar(ncout,varname,xtype,timedimid);      
+    elseif dimids==rdim
+      ovarid=netcdf.defVar(ncout,varname,xtype,[londimid latdimid]);
+    else
+      warning('Additional dimension of %s not implemented, skipped',varname);
+      continue;
+    end
+  elseif length(dimids)==2
+    if dimids(1)==rdim & dimids(2)==tdim
+      if (xtype==dtype) xtype=stype; end
+      ovarid=netcdf.defVar(ncout,varname,xtype,[londimid latdimid timedimid]);
+    else
+      warning('Additional dimension of %s not implemented, skipped',varname); 
+      continue;
+    end
+  else
+    warning('Additional dimension of %s not implemented, skipped',varname); 
+    continue;
+  end
+  for iatt=0:natts-1
+    attname = netcdf.inqAttName(ncid,varid,iatt);
+    netcdf.copyAtt(ncid,varid,attname,ncout,ovarid);
+  end
+  netcdf.putAtt(ncout,ovarid,'date_of_modification',datestr(now));  
+end
+netcdf.endDef(ncout);
+
+
+
+
+%% Read GLUES raster
+% map.latgrid is 89 . -89
+% map.longrid is -179.. 179
+% size(map.region) is 360 x 720
+
+load('regionmap_sea_685.mat');
+nrow=size(map.region,1);
+ncol=size(map.region,2);
+
+map.region=zeros(720,360);
+for i=1:length(land.region)
+    map.region(land.ilon(i),land.ilat(i))=land.region(i);
+end
+  
+% find ncell for each glues region
+for i=1:length(id)
+  ncell(i,1)=sum(land.region==i);
+end
+mapping=ones(nreg,max(ncell));
+for i=1:nreg
+  mapping(i,1:ncell(i))=find(map.region==i);
+end
+
+
+% Put new time records and lat/lon grid
+for i=1:length(time)
+  ovarid=netcdf.inqVarID(ncout,'time');
+  netcdf.putVar(ncout,ovarid,i-1,double(time(i)));
+end
+ovarid=netcdf.inqVarID(ncout,'lon');
+netcdf.putVar(ncout,ovarid,longrid);
+ovarid=netcdf.inqVarID(ncout,'lat');
+netcdf.putVar(ncout,ovarid,latgrid);
+
+tstride=round(timestep)/dtime;
+ntime=length(itime);
+[ndim nvar natt udimid] = netcdf.inq(ncout); 
+for ovarid=0:nvar-1
+  [varname,xtype,dimids,natts]=netcdf.inqVar(ncout,ovarid);        
+  switch varname
+    case {'time','lat','lon'}, continue;
+  end
+    
+  varid=netcdf.inqVarID(ncid,varname);
+  
+  if length(dimids)==3 & (dimids==[londimid latdimid timedimid])
+    data=netcdf.getVar(ncid,varid,[0 itime(1)-1],[nreg ntime],[1 tstride]);
+    mdata=zeros(nglon,nglat)-NaN;
+    rdata=zeros(nglon,nglat,ntime);
+    for j=1:ntime 
+      for i=1:nreg
+        mdata(mapping(i,:))=data(i,j);
+      end
+      mdata(1)=NaN;
+      rdata(:,:,j)=mdata;
+    end
+  elseif length(dimids)==2 & (dimids==[londimid latdimid])
+    data=netcdf.getVar(ncid,varid);
+    rdata=zeros(nglon,nglat)-NaN;
+    for i=1:nreg
+      rdata(mapping(i,:))=data(i);
+    end
+    rdata(1)=NaN;
+  else
+    warning('Writing of %s with this dimension not implemented. Skipped',varname);
+    continue;
+  end
+    
+  if (xtype==itype) rdata(isnan(rdata))=-9999; rdata=round(rdata); end 
+  if (xtype==stype) rdata=single(rdata); end 
+  if (xtype==dtype) rdata=double(rdata); end 
+
+  fprintf('Writing variable %s ...\n',varname);
+  netcdf.putVar(ncout,ovarid,rdata);
+end
+netcdf.close(ncout);
+netcdf.close(ncid);
+
+
+return
+end
--- visual/matlab/clp_distance_timing.m
+++ visual/matlab/clp_distance_timing.m
@@ -1,239 +1,155 @@
-function clp_distance_timing(varargin)
-
-cl_register_function();
+function retdata=clp_distance_timing(varargin)
 
 arguments = {...
-  {'vars','Farming'},...
-  {'modus','absolute'},...
-  {'resultfilename','results'},...
-  {'scenario',''},...
-  {'lonlim',[0,36]},...
-  {'timelim',[11000,3100]},...
-  {'timeunit','BP'},...
-  {'lonlim',[0,36]},...
-  {'figoffset',0}
+  {'timelim',[-8000,-4000]},...
+  {'file','../../eurolbk_base.nc'},...
+  {'data','neolithicsites.mat'},...
+  {'reg','lbk'},...
+  {'retdata',NaN},...
 };
 
+cl_register_function;
+
 [a,rargs]=clp_arguments(varargin,arguments);
-for i=1:a.length eval([a.name{i} '=' clp_valuestring(a.value{i}) ';']); end
-
-[d,f]=get_files;
-retdata=NaN;
-
-% Choose 'emea' or 'China' or 'World'
-[regs,nreg,lonlim,latlim]=find_region_numbers('EMEA','lon',lonlim);
-
-regs=regs(find(regs<300 & regs>120 & regs~=204));
-nreg=length(regs);
-hregs=[272,243,256,254,212,217,184,179,171,147,143,123,200]';
-
-figoffset=0;
-  
-if ~exist('timelim','var') timelim=[11000,3100]; end;
-if ~exist('timeunit','var') timeunit='BP'; end;
-if (length(scenario)>0)
-  resultfilename=[resultfilename '_' scenario];
+for i=1:a.length 
+  eval([a.name{i} '=' clp_valuestring(a.value{i}) ';']); 
 end
 
-load(['regionpath_' num2str(685)]);
-if ~exist('region','var')
-    region.path=regionpath;
-end
+%% Do Ammerman plot 
+figure(1); clf reset; hold on;
+set(gcf,'Units','centimeters','Position',[0 0 18 18]);
+% Single column 90 mm 1063 1772 3543 
+% 1.5 column 140 mm 1654 2756 5512
+% Full width 190 mm 2244 3740 7480
 
-if exist([resultfilename '.mat'],'file') load(resultfilename); else 
-    fprintf('Cannot find file %s\n',resultfilename);
-    error('No result file read')
-    return; 
-end
+ncid=netcdf.open(file,'NOWRITE');
+varid=netcdf.inqVarID(ncid,'region');
+region=netcdf.getVar(ncid,varid);
+varid=netcdf.inqVarID(ncid,'time');
+time=netcdf.getVar(ncid,varid);
+itime=find(time>=timelim(1) & time<=timelim(2));
+time=time(itime);
+varid=netcdf.inqVarID(ncid,'farming');
+farming=netcdf.getVar(ncid,varid);
+varid=netcdf.inqVarID(ncid,'latitude');
+lat=double(netcdf.getVar(ncid,varid));
+varid=netcdf.inqVarID(ncid,'longitude');
+lon=double(netcdf.getVar(ncid,varid));
+netcdf.close(ncid);
+nreg=length(region);
 
-infix=strrep(resultfilename,'result','');
-if length(infix)>0
-  while infix(1)=='_' infix=infix(2:end); end
-end
+% get lat/lon from regionpath, since they are wrong in the above file
+if ~exist('lonlat_685.mat','file')
+  load('regionpath_685');
+  regionpath(:,:,1)=regionpath(:,:,1)+0.5;
+  regionpath(:,:,2)=regionpath(:,:,2)+1;
+  lats=squeeze(regionpath(:,:,2));
+  lons=squeeze(regionpath(:,:,1));
 
-
-if iscell(vars) 
-    nvar=length(vars);
-for ivar=1:nvar
-        
-    m=strmatch(vars{ivar},r.variables,'exact');
-    if isempty(m)
-        switch(vars{ivar})
-            case 'Methane'
-          dovar(ivar)=length(r.variables)+1;
-          r.variables{dovar(ivar)}='Methane'
-          people=r.Density.*repmat(region.area',1,ntime);
-          farmers=people.*r.Farming;
-          r.Methane=methane_emission(farmers)/1E4;
-            case 'Cropfraction'
-               dovar(ivar)=length(r.variables)+1;
-           r.variables{dovar(ivar)}='Cropfraction';
-           load('region_hyde_685.mat');
-           r.Cropfraction=climate.crop;
-       
-            case 'HydePopulationDensity'
-               dovar(ivar)=length(r.variables)+1;
-           r.variables{dovar(ivar)}='HydePopulationDensity';
-           load('region_hyde_685.mat');
-           r.HydePopulationDensity=climate.popd;
-       
-            case 'HydePopulationCount'
-               dovar(ivar)=length(r.variables)+1;
-           r.variables{dovar(ivar)}='HydePopulationCount';
-           load('region_hyde_685.mat');
-           r.HydePopulationCount=climate.popc;
- 
-            case 'GluesRemainingForest'
-              dovar(ivar)=length(r.variables)+1;
-           r.variables{dovar(ivar)}='GluesRemainingForest';
-           load('hyde_glues_cropfraction.mat');
-           r.GluesRemainingForest=remainingforest;
-
-            case 'GluesCropfraction'
-              dovar(ivar)=length(r.variables)+1;
-           r.variables{dovar(ivar)}='GluesCropfraction';
-           load('hyde_glues_cropfraction.mat');
-           r.GluesCropfraction=cropfraction;
-          
-           case 'GluesNaturalForest'
-              dovar(ivar)=length(r.variables)+1;
-           r.variables{dovar(ivar)}='GluesNaturalForest';
-           load('hyde_glues_cropfraction.mat');
-           r.GluesNaturalForest=naturalforest;
-           case 'GluesDeforestation'
-              dovar(ivar)=length(r.variables)+1;
-           r.variables{dovar(ivar)}='GluesDeforestation';
-           load('hyde_glues_cropfraction.mat');
-           r.GluesDeforestation=deforestation/10E9;
-             otherwise 
-              warning('Variable "%s" not found in result file. Skipped',vars{ivar});
-        end
-        
-    else dovar(ivar)=m;
-    end
-end
-else nvar=1; 
-    m=strmatch(vars,r.variables,'exact');
-    if isempty(m)
-        warning('Variable "%s" not found in result file. Skipped',vars);
-    else dovar=m;
-    end
-end
-
- 
-if ~exist('dovar','var') return; end;
-
-dovar=dovar(find(dovar>0));
-nvar=length(dovar);
-
-isbp=1;
-if all(r.time>=0)
-  toffset=500;
-  tstart=min([r.tend+toffset,timelim(1)]);
-  tend=max([r.tstart+toffset,timelim(2)]);
-  r.time=r.time+toffset;
-else
-  tstart=max([r.tstart,2000-timelim(1)]);
-  tend=min([r.tend,2000-timelim(2)]);
-  timeunit='AD';
-  isbp=0;
-end
-
-
-time=r.time;
-    
-[tmax,itend]=min(abs(time-tend));
-[tmin,itstart]=min(abs(time-tstart));
-
-ntime=itend-itstart+1;
-
-retdata=zeros(nvar,ntime,length(regs))+NaN;
-
-nreg=length(regs);
-for idovar=1:nvar
-    
-    ivar=dovar(idovar);
-
-    
-   data=eval(['r.' r.variables{ivar}]);
-
-   retdata(idovar,:,:)=data(squeeze(regs),itstart:itend)';
-    
-   switch modus
-      case 'absolute', ;
-      otherwise ;
-   end 
-  
-   minmax=[min(min(min(data(regs,itstart:itend)))),max(max(max(data(regs,itstart:itend))))];
-   if exist('ylim','var') minmax=ylim; end
-   
-  
-  resvar=round(((data-minmax(1)))./(minmax(2)-minmax(1)));
-  
-
-  refreg=272;
-  irefreg=find(regs==refreg);
-  thresh=0.3;
-  for ireg=1:length(regs)
-      ftime=find(data(regs(ireg),:)>=thresh);
-      if isempty(ftime) threshtime(ireg)=NaN; 
-      else threshtime(ireg)=time(ftime(1)); end
-      
-      dist(ireg)=m_lldist([regioncenter(refreg,2),regioncenter(regs(ireg),2)],...
-      [regioncenter(refreg,1),regioncenter(regs(ireg),1)]);
-      
-  
+  for ir=1:nreg
+    lat(ir)=calc_geo_mean(lats(ir,:),lats(ir,:));
+    lon(ir)=calc_geo_mean(lats(ir,:),lons(ir,:));
   end
-  
-  % Plot timing-distance scatter plot
-  figure(ivar+nvar+figoffset); 
-  clf reset;
-   
-  hold on;
-  
-  y=regioncenter(regs,2); % longitude
-  y=dist; % distances to refreg
-  x=threshtime;
-  
-  for ireg=1:nreg
-     
-     if isempty(find(regs(ireg)==hregs))
-       plot(x(ireg),y(ireg),'r.');
-     else
-       plot(x(ireg),y(ireg),'m.');
-     end
-  end
-  y0=threshtime(irefreg);
-  
-  plot(y0:500:y0+4000,0:500:4000,'k-','color',[0.3 0.3 1.0],'linewidth',1.2);
-  [r,p]=corrcoef(x,y)
-  pol=polyfit(x,y,1);
-  fprintf('%f %f %f\n',r(2,1).*r(2,1),pol(:));
-  
-  text(x+20,y,num2str(regs));
-  xlabel('Time of agricultural onset (a BC)');
-  ylabel('Great circle distance from assumed center (km)');
-  set(gca,'Ylim',[-20,3800]);
-  
-  figure(ivar+nvar*2+figoffset);
-  clf reset;
-  hold on;
-  
-  for ireg=1:length(regs)
-      for jreg=ireg+1:length(regs);
-        rdist(ireg,jreg)=m_lldist([regioncenter(regs(ireg),2),regioncenter(regs(jreg),2)],...
-          [regioncenter(regs(ireg),1),regioncenter(regs(jreg),1)]); 
-        tdist(ireg,jreg)=abs(threshtime(ireg)-threshtime(jreg));
-      end
-  end
-   
-  for ireg=1:length(regs)-1
-      plot(tdist(ireg,ireg+1:end),rdist(ireg,ireg+1:end),'b.');
-  end
+  save('lonlat_685','lon','lat');
+else load('lonlat_685');
 end
-close(gcf);
+  
 
-plot_multi_format(gcf,'distance_timing');
 
-return;
+if ~exist('neolithicsites.mat','file')
+    fprintf('Required file neolithicsites.mat not found, please contact distributor.\n');
+    return
+end
+
+
+load('neolithicsites');
+slat=Forenbaher.Latitude;
+slon=Forenbaher.Long;
+period=Forenbaher.Period;
+site=Forenbaher.Site_name;
+sage=Forenbaher.Median_age;
+ns=length(sage);
+dlat=[slat' repmat(lat(272),ns,1)];
+dlat=reshape(dlat',2*ns,1);
+dlon=[slon' repmat(lon(272),ns,1)];
+dlon=reshape(dlon',2*ns,1);
+sdists=m_lldist(dlon,dlat);
+sdists=sdists(1:2:end);
+
+[ifound,nfound,lonlim,latlim]=find_region_numbers(reg);
+farming=farming(ifound,itime);
+
+ncol=19;
+cmap=flipud(jet(ncol));
+stime=1950-sage;
+sutime=stime+(Forenbaher.Lower_cal-Forenbaher.Median_age);
+sltime=stime+(Forenbaher.Upper_cal_-Forenbaher.Median_age);
+srange=sutime-sltime;
+iscol=floor((stime-min(timelim))./(timelim(2)-timelim(1))*ncol)+1;
+viscol=find(iscol>0 & iscol<=ncol & slat>=latlim(1) & slat<=latlim(2) ...
+    & slon>=lonlim(1) & slon<=lonlim(2));
+  
+
+
+
+i272=find(ifound==272);
+
+dlat=[lat repmat(lat(272),nreg,1)];
+dlat=reshape(dlat',2*nreg,1);
+dlon=[lon repmat(lon(272),nreg,1)];
+dlon=reshape(dlon',2*nreg,1);
+dists=m_lldist(dlon,dlat);
+dists=dists(1:2:end);
+dists=dists(ifound);
+threshold=0.5;
+it=(farming>=threshold).*repmat(1:length(itime),nfound,1);
+it(it==0)=inf;
+it=min(it,[],2);
+itv=isfinite(it);
+onset=zeros(nfound,1)+inf;
+onset(itv)=time(min(it(itv),[],2));
+
+gtime=timelim(1):200:timelim(2);
+
+set(gca,'color','none','FontSize',14,'FontName','Times');
+p1=plot(-stime,sdists,'bo','MarkerFaceColor','none','MarkerSize',2);
+ylabel('Distance from Levante (km)');
+xlabel('Time (year BC)'); 
+set(gca,'Xlim',-fliplr([timelim]));
+set(gca,'XDir','reverse');
+p2=plot(-onset,dists,'rd','MarkerFaceColor','r','MarkerSize',5)
+%plot(7500:-200:3500,0:200:4000,'k--');
+
+pf1=polyfit(-onset(itv),dists(itv),1);
+p4=plot(-gtime,-gtime*pf1(1)+pf1(2),'r-','LineWidth',2);
+pf2=polyfit(-stime(viscol)',sdists(viscol),1);
+% Indistiguishable, thus not shown
+%plot(-gtime,-gtime*pf2(1)+pf2(2),'b--','MarkerSize',2);
+
+[cr1,cp1]=corrcoef(onset(itv),dists(itv));
+[cr2,cp2]=corrcoef(stime(viscol),sdists(viscol));
+
+ytl=get(gca,'YTickLabel');
+ytl(1,:)=' ';
+set(gca,'YTickLabel',ytl);
+
+l=legend([p1,p2],sprintf('Site data (n=%d, r^2=%.2f, v=%.2f km a^{-1})',length(viscol),cr2(2).^2,-pf2(1)),...
+    sprintf('Simulation (n=%d, r^2=%.2f, v=%.2f km a^{-1})',length(itv),cr1(2).^2,-pf1(1)),...
+    'location','NorthWest','FontSize',7);
+%set(l,'color','none');
+
+[fp fn fe]=fileparts(file);
+
+plot_multi_format(1,['distance_timing_rgb_' fn],'pdf');
+
+
+%% Do black and white version
+%set([p2,p1,p4],'Color',repmat(0,3,1));
+%set(p2,'MarkerFaceColor',repmat(0,3,1));
+%plot_multi_format(1,'dinstance_timing_bw','pdf');
+if nargout>0
+   retdata=[cr1(2),cp1(2),-pf1(1)]
+end
+
+return
 end
