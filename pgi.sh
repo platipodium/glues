c35c6bccaa6daf7ca60d4dc61f57ad3257c3d4e9
Carsten Lemmen <carsten.lemmen@hzg.de>
2012-01-21 11:49:30 +0100

    Adjusted paths to sisi libs

--- pgi.sh
+++ pgi.sh
@@ -1,6 +1,6 @@
 ./bootstrap && ./configure CC=pgcc CXX=pgCC \
---with-sisi-include=$HOME/opt/src/SiSi2.2/sisi/include \
---with-sisi-lib=$HOME/opt/src/SiSi2.2/sisi/lib-pgi \
+--with-sisi-include=$HOME/opt/src/sisi/include \
+--with-sisi-lib=$HOME/opt/src/sisi/lib-pgi \
 && (cd src ; gmake -k clean ) 
 gmake -k -j4 || (cd src gmake -k j4)
  ./run.sh
