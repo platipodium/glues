56ae5ba16ed940dada6d22e769e68c58b00f3086
Carsten Lemmen <carsten.lemmen@hzg.de>
2012-01-27 15:08:26 +0100

    Fixed netcdf path for intel compiler

--- patch.sh
+++ patch.sh
@@ -1,8 +1,13 @@
 #/bin/bash
 # Patch current bugs
 
+if test -x gsed ; then SED=gsed; 
+elif test -x sed ; then SED=sed;
+else SED=sed
+fi
+
 # 1. Patch dynamic lib of sisi to static lib
 #for F in src/Makefile src/region/Makefile src/variation/Makefile ; do
-gsed -i '/LIBS =/s#-lSiSi#sisi/lib/.libs/libSiSi.a#' src/Makefile; 
+${SED} -i '/LIBS =/s#-lSiSi#sisi/lib/.libs/libSiSi.a#' src/Makefile; 
 #done
 
