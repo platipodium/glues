dbc304187a77dba1c9236d8499554f19f5c25520
Carsten Lemmen <carsten.lemmen@hzg.de>
2012-06-13 15:40:44 +0200

    Fixed 360 year conversion here.  Look out for more errors whenever time is read/written from netcdf

--- run-euroclim.sh
+++ run-euroclim.sh
@@ -29,6 +29,10 @@ OPAR=$P.opar
 R=$S/results
 export PAR=$P.par
 
+echo Simulation: $SIM >> $L
+echo Results: $R >> $L
+
+
 # Make sure we start from base setup (with spread, no fluc)
 $SED -i '/spreadv/s/spreadv.*$/spreadv 0.002/' $PAR
 $SED -i '/spreadm/s/spreadm.*$/spreadm 100/' $OPAR
@@ -59,11 +63,12 @@ for (( i=4 ; i<5; i=i+1 )) ; do
   $SED -i '/flucampl/s/flucampl.*$/flucampl '$SCE'/' $DAT
   $SED -i '/gammab/s/gammab.*$/gammab 0.00403/' $PAR
   LOG=${B}_${SCE}.log 
+  NCIN=${SIM%%.sim}.nc
   NC=${LOG%%.log}.nc
   echo Running: $X $SIM 
-  echo File output: $NC >> $L
+  echo File output: $NCIN ' -> '$NC >> $L
   $X $SIM 2> ${LOG} && \
-  ncks -O -v time,region_neighbour,natural_fertility,temperature_limitation,population_size,population_density,technology,economies,farming,region,latitude,longitude,area,migration_density test.nc ${NC}
+  ncks -O -v time,region_neighbour,natural_fertility,temperature_limitation,population_size,population_density,technology,economies,farming,region,latitude,longitude,area,migration_density ${NCIN} ${NC}
   test -f ${LOG} && $SED -i '/[A-d]/d' ${LOG}
 done
 
@@ -80,11 +85,12 @@ for (( i=0 ; i<1; i=i+1 )) ; do
   $SED -i '/flucampl/s/flucampl.*$/flucampl '$SCE'/' $DAT
   $SED -i '/gammab/s/gammab.*$/gammab 0.00400/' $PAR
    LOG=${B}_${SCE}.log 
+  NCIN=${SIM%%.sim}.nc
   NC=${LOG%%.log}.nc
   echo Running: $X $SIM 
-  echo File output: $NC >> $L
+  echo File output: $NCIN ' -> '$NC >> $L
   $X $SIM 2> ${LOG} && \
-  ncks -O -v time,region_neighbour,natural_fertility,temperature_limitation,population_size,population_density,technology,economies,farming,region,latitude,longitude,area,migration_density test.nc ${NC}
+  ncks -O -v time,region_neighbour,natural_fertility,temperature_limitation,population_size,population_density,technology,economies,farming,region,latitude,longitude,area,migration_density ${NCIN} ${NC}
   test -f ${LOG} && $SED -i '/[A-d]/d' ${LOG}
 done
 
@@ -94,10 +100,11 @@ done
 SCE=1.0
 $SED -i '/flucampl/s/flucampl.*$/flucampl '$SCE'/' $DAT
 LOG=${B}_${SCE}.log 
-NC=${LOG%%.log}.nc
+  NCIN=${SIM%%.sim}.nc
+  NC=${LOG%%.log}.nc
   echo Running: $X $SIM 
-  echo File output: $NC >> $L
-#$X $SIM 2> ${LOG} && mv test.nc $NC
+  echo File output: $NCIN ' -> '$NC >> $L
+#$X $SIM 2> ${LOG} && mv $NCIN  $NC
 test -f ${LOG} && $SED -i '/[A-d]/d' ${LOG}
 
 # Return to base setup
@@ -105,255 +112,3 @@ test -f ${LOG} && $SED -i '/[A-d]/d' ${LOG}
 
 exit
 
-
-test -f ${LOG} && $SED -i '/[A-d]/d' ${LOG}
-
-
-
-exit
-
-
-
-
-
-
-
-
-# No trade, no migration
-  SV=$(echo "scale=10; 0." | bc )
-  SM=$(echo "scale=10; 0" | bc )
-  SC=$(echo "scale=10; $SM*$SV" | bc )
-  
-  
-  SVI=0${SV%.*}
-  SVF=${SV#*.}
-  SV=$(printf "%09.7f\n" "$SVI,$SVF")
-  SVI=${SV%,*}
-  SVF=${SV#*,}
-
-  SMI=0${SM%.*}
-  SMF=${SM#*.}
-  SM=$(printf "%010.2f\n" "$SMI,$SMF")
-  SMI=${SM%,*}
-  SMF=${SM#*,}
-  
-  SCI=0${SC%.*}
-  SCF=${SC#*.}
-  SC=$(printf "%010.6f\n" "$SCI,$SCF")
-  SCI=${SC%,*}
-  SCF=${SC#*,}
-  
-  LOG=${V}_${SVI}.${SVF}_${SMI}.${SMF}_${SCI}.${SCF}_.log 
-  NC=${V}_${SVI}.${SVF}_${SMI}.${SMF}_${SCI}.${SCF}_.nc 
-
-  VEXPR=''"/spreadv/s/spreadv.*$/spreadv ${SVI}.${SVF}"/'' 
-  MEXPR=''"/spreadm/s/spreadm.*$/spreadm ${SMI}.${SMF}"/'' 
-  $SED -i "$VEXPR" $PAR
-  $SED -i "$MEXPR" $OPAR
-
-test -f ${LOG} ||   $X $SIM 2> ${LOG} && ncks -O -v time,population_density,farming,region,latitude,longitude test.nc ${NC}
-test -f ${LOG} && $SED -i '/[A-d]/d' ${LOG}
-
-
-exit
-# Vary importance of trade and migration by  applying equal factors
-for (( i=-8; i<=8; i++ )) ; do
-  SV=$(echo "scale=10; 0.002*0.5^$i" | bc )
-  SM=$(echo "scale=10; 100*4^$i" | bc )
-  SC=$(echo "scale=10; $SM*$SV" | bc )
-  
-  SVI=0${SV%.*}
-  SVF=${SV#*.}
-  SV=$(printf "%09.7f\n" "$SVI,$SVF")
-  SVI=${SV%,*}
-  SVF=${SV#*,}
-
-  SMI=0${SM%.*}
-  SMF=${SM#*.}
-  SM=$(printf "%010.2f\n" "$SMI,$SMF")
-  SMI=${SM%,*}
-  SMF=${SM#*,}
-  
-  SCI=0${SC%.*}
-  SCF=${SC#*.}
-  SC=$(printf "%010.6f\n" "$SCI,$SCF")
-  SCI=${SC%,*}
-  SCF=${SC#*,}
-  
-  LOG=${V}_${SVI}.${SVF}_${SMI}.${SMF}_${SCI}.${SCF}_.log 
-  NC=${V}_${SVI}.${SVF}_${SMI}.${SMF}_${SCI}.${SCF}_.nc 
-
-  VEXPR=''"/spreadv/s/spreadv.*$/spreadv ${SVI}.${SVF}"/'' 
-  MEXPR=''"/spreadm/s/spreadm.*$/spreadm ${SMI}.${SMF}"/'' 
-  $SED -i "$VEXPR" $PAR
-  $SED -i "$MEXPR" $OPAR
-
-  test -f ${LOG} && continue
-  $X $SIM 2> ${LOG} && ncks -O -v time,population_density,farming,region,latitude,longitude test.nc ${NC}
-  test -f ${LOG} && $SED -i '/[A-d]/d' ${LOG}
-
-done
-
-
-# Changing migratino around constant trade (keep spreadm*spreadv constant)
-for (( i=-10; i<=10; i++ )) ; do
-  SV=$(echo "scale=10; 0.002*0.5^$i" | bc )
-  SM=$(echo "scale=10; 100*2^$i" | bc )
-  SC=$(echo "scale=10; $SM*$SV" | bc )
-  
-  SVI=0${SV%.*}
-  SVF=${SV#*.}
-  SV=$(printf "%09.7f\n" "$SVI,$SVF")
-  SVI=${SV%,*}
-  SVF=${SV#*,}
-
-  SMI=0${SM%.*}
-  SMF=${SM#*.}
-  SM=$(printf "%010.2f\n" "$SMI,$SMF")
-  SMI=${SM%,*}
-  SMF=${SM#*,}
-  
-  SCI=0${SC%.*}
-  SCF=${SC#*.}
-  SC=$(printf "%010.6f\n" "$SCI,$SCF")
-  SCI=${SC%,*}
-  SCF=${SC#*,}
-  
-  
-  LOG=${V}_${SVI}.${SVF}_${SMI}.${SMF}_${SCI}.${SCF}_.log 
-  NC=${V}_${SVI}.${SVF}_${SMI}.${SMF}_${SCI}.${SCF}_.nc 
-
-  VEXPR=''"/spreadv/s/spreadv.*$/spreadv ${SVI}.${SVF}"/'' 
-  MEXPR=''"/spreadm/s/spreadm.*$/spreadm ${SMI}.${SMF}"/'' 
-  $SED -i "$VEXPR" $PAR
-  $SED -i "$MEXPR" $OPAR
-
-  test -f ${LOG} && continue
-  $X $SIM 2> ${LOG} && ncks -O -v time,population_density,farming,region,latitude,longitude test.nc ${NC}
-  test -f ${LOG} && $SED -i '/[A-d]/d' ${LOG}
-done
-
-# Change migration around no trade
-for (( i=-9; i<=11; i++ )) ; do
-  SV=$(echo "scale=7; 0.002*2^$i" | bc )
-  SM=$(echo "scale=2; 0.0" | bc )
-  SC=$(echo "scale=7; $SM*$SV" | bc )
-  
-  SVI=0${SV%.*}
-  SVF=${SV#*.}
-  SV=$(printf "%09.7f\n" "$SVI,$SVF")
-  SVI=${SV%,*}
-  SVF=${SV#*,}
-
-  SMI=0${SM%.*}
-  SMF=${SM#*.}
-  SM=$(printf "%010.2f\n" "$SMI,$SMF")
-  SMI=${SM%,*}
-  SMF=${SM#*,}
-  
-  
-  SCI=0${SC%.*}
-  SCF=${SC#*.}
-  SC=$(printf "%010.6f\n" "$SCI,$SCF")
-  SCI=${SC%,*}
-  SCF=${SC#*,}
-  
-
-  LOG=${V}_${SVI}.${SVF}_${SMI}.${SMF}_${SCI}.${SCF}_.log 
-  NC=${V}_${SVI}.${SVF}_${SMI}.${SMF}_${SCI}.${SCF}_.nc 
-
-  VEXPR=''"/spreadv/s/spreadv.*$/spreadv ${SVI}.${SVF}"/'' 
-  MEXPR=''"/spreadm/s/spreadm.*$/spreadm ${SMI}.${SMF}"/'' 
-  $SED -i "$VEXPR" $PAR
-  $SED -i "$MEXPR" $OPAR
-
-  test -f ${LOG} && continue
-  $X $SIM 2> ${LOG} && ncks -O -v time,population_density,farming,region,latitude,longitude test.nc ${NC}
-  test -f ${LOG} && $SED -i '/[A-d]/d' ${LOG}
-done
-
-# Trait variation around minimal migration
-for (( i=-9; i<=18; i++ )) ; do
-  SV=$(echo "scale=7; 0.0000002" | bc )
-  SM=$(echo "scale=2; 100*2^$i" | bc )
-  SC=$(echo "scale=7; $SM*$SV" | bc )
-  
-  SVI=0${SV%.*}
-  SVF=${SV#*.}
-  SV=$(printf "%09.7f\n" "$SVI,$SVF")
-  SVI=${SV%,*}
-  SVF=${SV#*,}
-
-  SMI=0${SM%.*}
-  SMF=${SM#*.}
-  SM=$(printf "%010.2f\n" "$SMI,$SMF")
-  SMI=${SM%,*}
-  SMF=${SM#*,}
-  
-  
-  SCI=0${SC%.*}
-  SCF=${SC#*.}
-  SC=$(printf "%010.6f\n" "$SCI,$SCF")
-  SCI=${SC%,*}
-  SCF=${SC#*,}
-  
-
-  LOG=${V}_${SVI}.${SVF}_${SMI}.${SMF}_${SCI}.${SCF}_.log 
-  NC=${V}_${SVI}.${SVF}_${SMI}.${SMF}_${SCI}.${SCF}_.nc 
-
-  VEXPR=''"/spreadv/s/spreadv.*$/spreadv ${SVI}.${SVF}"/'' 
-  MEXPR=''"/spreadm/s/spreadm.*$/spreadm ${SMI}.${SMF}"/'' 
-  $SED -i "$VEXPR" $PAR
-  $SED -i "$MEXPR" $OPAR
-
-  test -f ${LOG} && continue
-  $X $SIM 2> ${LOG} && ncks -O -v time,population_density,farming,region,latitude,longitude test.nc ${NC}
-  test -f ${LOG} && $SED -i '/[A-d]/d' ${LOG}
-done
-
-
-# Trait variation around medium migration
-for (( i=-10; i<=10; i++ )) ; do
-  SV=$(echo "scale=7; 0.002" | bc )
-  SM=$(echo "scale=2; 100*2^$i" | bc )
-  SC=$(echo "scale=7; $SM*$SV" | bc )
-  
-  SVI=0${SV%.*}
-  SVF=${SV#*.}
-  SV=$(printf "%09.7f\n" "$SVI,$SVF")
-  SVI=${SV%,*}
-  SVF=${SV#*,}
-
-  SMI=0${SM%.*}
-  SMF=${SM#*.}
-  SM=$(printf "%010.2f\n" "$SMI,$SMF")
-  SMI=${SM%,*}
-  SMF=${SM#*,}
-  
-  
-  SCI=0${SC%.*}
-  SCF=${SC#*.}
-  SC=$(printf "%010.6f\n" "$SCI,$SCF")
-  SCI=${SC%,*}
-  SCF=${SC#*,}
-  
-
-  LOG=${V}_${SVI}.${SVF}_${SMI}.${SMF}_${SCI}.${SCF}_.log 
-  NC=${V}_${SVI}.${SVF}_${SMI}.${SMF}_${SCI}.${SCF}_.nc 
-
-  VEXPR=''"/spreadv/s/spreadv.*$/spreadv ${SVI}.${SVF}"/'' 
-  MEXPR=''"/spreadm/s/spreadm.*$/spreadm ${SMI}.${SMF}"/'' 
-  $SED -i "$VEXPR" $PAR
-  $SED -i "$MEXPR" $OPAR
-
-  test -f ${LOG} && continue
-  $X $SIM 2> ${LOG} && ncks -O -v time,population_density,farming,region,latitude,longitude test.nc ${NC}
-  test -f ${LOG} && $SED -i '/[A-d]/d' ${LOG}
-done
-
-
-# return to base setup
-$SED -i '/spreadv/s/spreadv.*$/spreadv 0.002/' $PAR
-$SED -i '/spreadm/s/spreadm.*$/spreadm 100/' $OPAR
-$SED -i '/LocalSpread/s/LocalSpread.*$/LocalSpread 1/' $CTL
-$SED -i '/flucampl/s/flucampl.*$/flucampl 0.0/' $DAT
